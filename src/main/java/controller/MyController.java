874cf8f50d6dc6f14629c66428b80329d20b7de3
heart-original <1316305052@qq.com>
2020-06-28 22:42:37 +0800

    ajax

--- src/main/java/controller/MyController.java
+++ src/main/java/controller/MyController.java
@@ -1,43 +1,84 @@
 package controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import domain.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class MyController {
 
-    @RequestMapping(value = "/receiveproperty.do")
-    public ModelAndView doSome(String name , Integer age){
-        System.out.println("doSome, name="+name+"   age="+age);
-        ModelAndView mv=new ModelAndView();
-        mv.addObject("myname",name);
-        mv.addObject("myage",age);
-        mv.setViewName("show");
-        return mv;
+    @RequestMapping(value = "/returnVoid-ajax.do")
+    public void doReturnVoidAjax(HttpServletResponse response, String name, Integer age) throws IOException {
+        System.out.println("===doReturnVoidAjax====, name="+name+"   age="+age);
+        //处理ajax， 使用json做数据的格式
+        //service调用完成了， 使用Student表示处理结果
+        Student student  = new Student();
+        student.setName(name);
+        student.setAge(age);
+
+        String json = "";
+        //把结果的对象转为json格式的数据
+        if( student != null){
+            ObjectMapper om  = new ObjectMapper();
+            json  = om.writeValueAsString(student);
+            System.out.println("student转换的json===="+json);
+        }
+
+        //输出数据，响应ajax的请求
+        response.setContentType("application/json;charset=utf-8");
+        PrintWriter pw  = response.getWriter();
+        pw.println(json);
+        pw.flush();
+        pw.close();
+
     }
 
-    @RequestMapping(value = "/receiveparam.do")
-    public ModelAndView receiveParam(@RequestParam(value = "rname" , required = false) String name ,
-                                     @RequestParam(value = "rage" , required = false) Integer age){
-        System.out.println("doSome, name="+name+"   age="+age);
-        ModelAndView mv=new ModelAndView();
-        mv.addObject("myname",name);
-        mv.addObject("myage",age);
-        mv.setViewName("show");
-        return mv;
+    @RequestMapping(value = "/returnStudentJson.do")
+    @ResponseBody
+    public Student doStudentJsonObject(String name, Integer age) {
+
+        Student student = new Student();
+        student.setName("李四同学");
+        student.setAge(20);
+        return student; // 会被框架转为json
+
     }
 
-    @RequestMapping(value = "receiveobject.do")
-    public ModelAndView receiveParam( Student myStudent){
-        System.out.println("receiveParam, name="+myStudent.getName()+"   age="+myStudent.getAge());
-        ModelAndView mv=new ModelAndView();
-        mv.addObject("myname",myStudent.getName());
-        mv.addObject("myage",myStudent.getAge());
-        mv.addObject("mystudent",myStudent);
-        mv.setViewName("show");
-        return mv;
+    @RequestMapping(value = "/returnStudentJsonArray.do")
+    @ResponseBody
+    public List<Student> doStudentJsonObjectArray(String name, Integer age) {
+
+        List<Student> list = new ArrayList<>();
+
+        Student student = new Student();
+        student.setName("李四同学");
+        student.setAge(20);
+        list.add(student);
+
+        student = new Student();
+        student.setName("张三");
+        student.setAge(28);
+        list.add(student);
+
+
+        return list;
+
     }
+
+    @RequestMapping(value = "/returnStringData.do",produces = "text/plain;charset=utf-8")
+    @ResponseBody
+    public String doStringData(String name,Integer age){
+        return "Hello SpringMVC 返回对象，表示数据";
+    }
+
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -2,30 +2,39 @@
 <html>
 <head>
     <title>Title</title>
+    <script type="text/javascript" src="js/jquery-3.4.1.js"></script>
+    <script type="text/javascript">
+        $(function () {
+            $("button").click(function () {
+                //alert("button click");
+                $.ajax({
+                    //url:"returnVoid-ajax.do",
+                    //url:"/returnStudentJson.do",
+                    //url:"returnStudentJsonArray.do",
+                    url:"returnStringData.do",
+                    data:{
+                        name:"张三",
+                        age:20
+                    },
+                    type:"post",
+                    dataType:"text",
+                    //dataType:"json",
+                    success:function (resp) {
+                        //alert(resp.name+"   ===   "+resp.age);
+
+                        // $.each(resp , function (i , n) {
+                        //     alert(n.name + "======" + n.age);
+                        // })
+
+                        alert("返回的是文本数据："+resp);
+                    }
+                })
+            })
+        })
+    </script>
 </head>
 <body>
-<p>提交参数给Controller</p>
-
-<form action="receiveproperty.do" method="post">
-    姓名：<input type="text" name="name"> <br/>
-    年龄：<input type="text" name="age"> <br/>
-    <input type="submit" value="提交参数">
-</form>
-
 <br/>
-<p>请求参数名和处理器方法的形参名不一样</p>
-<form action="receiveparam.do" method="post">
-    姓名：<input type="text" name="rname"> <br/>
-    年龄：<input type="text" name="rage"> <br/>
-    <input type="submit" value="提交参数">
-</form>
-<br/>
-<p>使用java对象接收请求参数</p>
-<form action="receiveobject.do" method="post">
-    姓名：<input type="text" name="name"> <br/>
-    年龄：<input type="text" name="age"> <br/>
-    <input type="submit" value="提交参数">
-</form>
-
+<button id="btn">发起ajax请求</button>
 </body>
 </html>
