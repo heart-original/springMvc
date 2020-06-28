7b4415fce2d2f4acd9c48862f8558059981d8cd5
heart-original <1316305052@qq.com>
2020-06-28 21:22:35 +0800

    param and filter

--- src/main/java/controller/MyController.java
+++ src/main/java/controller/MyController.java
@@ -1,28 +1,43 @@
 package controller;
 
+import domain.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MyController {
 
-    @RequestMapping(value = "/some.do")
-    public ModelAndView doSome(){
+    @RequestMapping(value = "/receiveproperty.do")
+    public ModelAndView doSome(String name , Integer age){
+        System.out.println("doSome, name="+name+"   age="+age);
         ModelAndView mv=new ModelAndView();
-        mv.addObject("msg","springmvc:hello world");
-        mv.addObject("fun","执行的是doSome（）方法");
+        mv.addObject("myname",name);
+        mv.addObject("myage",age);
         mv.setViewName("show");
         return mv;
     }
 
-    @RequestMapping(value = "/other.do")
-    public ModelAndView doOther(){
+    @RequestMapping(value = "/receiveparam.do")
+    public ModelAndView receiveParam(@RequestParam(value = "rname" , required = false) String name ,
+                                     @RequestParam(value = "rage" , required = false) Integer age){
+        System.out.println("doSome, name="+name+"   age="+age);
         ModelAndView mv=new ModelAndView();
-        mv.addObject("msg","springmvc:hello world");
-        mv.addObject("fun","执行的是doOther（）方法");
-        mv.setViewName("other");
+        mv.addObject("myname",name);
+        mv.addObject("myage",age);
+        mv.setViewName("show");
         return mv;
     }
 
+    @RequestMapping(value = "receiveobject.do")
+    public ModelAndView receiveParam( Student myStudent){
+        System.out.println("receiveParam, name="+myStudent.getName()+"   age="+myStudent.getAge());
+        ModelAndView mv=new ModelAndView();
+        mv.addObject("myname",myStudent.getName());
+        mv.addObject("myage",myStudent.getAge());
+        mv.addObject("mystudent",myStudent);
+        mv.setViewName("show");
+        return mv;
+    }
 }
